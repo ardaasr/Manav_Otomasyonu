ade56919f832ccf480c1a997c000673700493305
ardaasr <arda.sari4444@icloud.com>
2024-10-19 00:38:20 +0300

    Add project files.

---
+++ Manav_Otomasyonu/Meyve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manav_Otomasyonu
+{
+    internal class Meyve
+    {
+        public string Ad { get; set; }
+        public int Stok { get; set; }
+        public double Fiyat { get; set; }
+    }
+}
